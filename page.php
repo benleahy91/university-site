819fd5c54a04b6ee3079307f9d8d096c566e823f
benleahy91 <benleahy91@gmail.com>
2019-05-03 18:52:45 -0400

    added dynamic blog

--- page.php
+++ page.php
@@ -3,7 +3,8 @@
   get_header();
 
   while(have_posts()) {
-    the_post(); ?>
+		the_post(); ?>
+	
 		<div class="page-banner">
 			<div class="page-banner__bg-image" style="background-image: url(<?php echo get_theme_file_uri('/images/ocean.jpg')?>);"></div>
 			<div class="page-banner__content container container--narrow">
@@ -19,7 +20,10 @@
 			$theParent = wp_get_post_parent_id(get_the_ID());
 			if ($theParent) { ?>
 				<div class="metabox metabox--position-up metabox--with-home-link">
-					<p><a class="metabox__blog-home-link" href="<?php echo get_permalink($theParent);?>"><i class="fa fa-home" aria-hidden="true"></i> Back to <?php echo get_the_title($theParent);?></a> <span class="metabox__main"><?php the_title(); ?></span></p>
+					<p>
+						<a class="metabox__blog-home-link" href="<?php echo get_permalink($theParent);?>">
+							<i class="fa fa-home" aria-hidden="true"></i> Back to <?php echo get_the_title($theParent);?>
+						</a> <span class="metabox__main"><?php the_title(); ?></span></p>
 				</div>
 			<?php } ?>
 			<?php
